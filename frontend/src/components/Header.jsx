f51175873d3628e1d322c7ea6394ff83d33e1f3b
mohamedaliarayssi680-dotcom <mohamedaliarayssi680@gmail.com>
2025-10-03 13:09:31 +0200

    Mise à jour du composant Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,9 @@ const Header = ({ title, logoUrl, theme, setTheme }) => {
 
   return (
     <header className="relative bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700 flex items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
-      {/* Titre (à gauche) */}
       <h1 className="text-2xl font-semibold text-gray-100">{title}</h1>
 
       <div className="flex items-center space-x-4">
-        {/* 🔹 Bouton Toggle */}
         <button
           onClick={() => setTheme(isDark ? "light" : "dark")}
           className="px-3 py-1 rounded bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
